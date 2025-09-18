73bf65c366f8f63d764951440d0c9f5de5b99266
KamolineR <164529564+KamolineR@users.noreply.github.com>
2025-09-18 12:06:40 -0300

    Delete models directory

--- models/database.js
+++
@@ -1,7 +0,0 @@
-const Sequelize = require ('sequelize')
-const connection = new Sequelize('PVO','root','',{
-    host: 'localhost',
-    dialect: 'mysql',
-    logging: false
-});
-module.exports = connection;
